2a46c0ed5e46847d5ab2d507a0410466fcdd0595
prs-dev <pratyushsri.25@gmail.com>
2026-04-26 20:01:21 +0530

    login working in client

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,14 +1,32 @@
 import {useState} from 'react'
+import useApi from './hooks/useApi'
+import { useUserContext } from './UserContext'
 
 const Login = () => {
   const [state, setState] = useState(null)
+
+  const {loginUser} = useApi()
+
+  const {token, setToken} = useUserContext()
 
   const handleChange = e => {
     setState(prev => ({...prev, [e.target.name]: e.target.value}))
   }
 
+const handleSubmit = async e => {
+  e.preventDefault()
+  const data = await loginUser(state)
+  if(data) {
+    console.log(data)
+    setState(null)
+    setToken(data?.token)
+  }
+}
+
+console.log("token", token)
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="">Email</label>
         <input type="email" name="email" value={state?.email} onChange={handleChange}/>
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 export const UserContext = createContext()
 
 export const UserContextProvider = ({children}) => {
+    const [token, setToken] = useState('')
     return (
-        <UserContext.Provider value="test">{children}</UserContext.Provider>
+        <UserContext.Provider value={{token, setToken}}>{children}</UserContext.Provider>
     )
+}
+
+export const useUserContext = () => {
+    const {token, setToken} = useContext(UserContext)
+    return {
+        token,
+        setToken
+    }
 }
 
