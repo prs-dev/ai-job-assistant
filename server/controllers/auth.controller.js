77d7b34f785bad2e023c368a5bd33f03cb9873ce
prs-dev <pratyushsri.25@gmail.com>
2026-04-26 09:59:26 +0530

    created register and login endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,41 @@
-const register = (req, res) => {
-    res.send('you are in register')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+        if (!name || !email || !password) return res.status(400).json({ msg: "please provide all fields!" })
+        const userExist = await User.findOne({ email })
+        if (userExist) return res.status(400).json({ msg: "user already exists, please login" })
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({
+            name, email, password: hashedPassword
+        })
+        await newUser.save()
+        delete newUser._doc.password
+        return res.status(201).json({ msg: "user created", user: newUser })
+    } catch (error) {
+        console.log("error in registering user", error)
+    }
 }
 
-const login = (req, res) => {
-     res.send('you are in login')
+const login = async(req, res) => {
+    try {
+        const {email, password} = req.body
+        if(!email || !password) return res.status(400).json({msg: "please provide all fields!"})
+        const user = await User.findOne({email})
+        if(!user) return res.status(400).json({msg: "Invalid credentials"})
+        const verifyPassword = await bcrypt.compare(password, user.password)
+        if(!verifyPassword) return res.status(400).json({msg: "Invalid credentials"})
+        const token = jwt.sign({_id: user._id}, process.env.SECRET)
+        return res.status(200).json({msg: "user logged in", token})
+    } catch (error) {
+        console.log("error in logging in user", error)
+    }
 }
 
 module.exports = {
-    register, 
+    register,
     login
 }
