28ea85847ae91d6cd059b74bd0b0e89f059547e4
prs-dev <pratyushsri.25@gmail.com>
2026-04-26 10:10:20 +0530

    added verify token

--- server/controllers/job.controller.js
+++ server/controllers/job.controller.js
@@ -2,6 +2,10 @@ const allJobs = (req, res) => {
     res.send("you are in all jobs")
 }
 
+const createJob = (req, res) => {
+    
+}
+
 module.exports = {
     allJobs
 }
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,6 +1,13 @@
+const jwt = require('jsonwebtoken')
+require("dotenv").config()
+
 const validToken = (req, res, next) => {
-    console.log("valid token")
-    next()
+    const token = req.headers.authorization?.split(" ")[1]
+    const verifyToken = jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if(err) return res.status(400).json({msg: "Invalid token"})
+        return decoded
+    })
+    if(verifyToken) next()
 }
 
 module.exports = {
