53d2c80a0d07e3ade280b39b6a43cec31489a77b
prs-dev <pratyushsri.25@gmail.com>
2026-04-27 14:52:50 +0530

    added details page

--- client/src/DetailsPage.jsx
+++ client/src/DetailsPage.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 import { useJobContext } from './context/JobContext'
 
 const DetailsPage = () => {
-  const {jobs} = useJobContext()
+  const { jobs } = useJobContext()
   console.log("jobs", jobs)
   return (
-    <div>DetailsPage</div>
+    <div>
+      {jobs?.map(job => {
+        return (
+          <>
+          <div>
+            <label>Company</label>
+            <p>{job?.company}</p>
+          </div>
+          <div>
+            <label>Role</label>
+            <p>{job?.role}</p>
+          </div>
+          <div>
+            <label>Status</label>
+            <p>{job?.status}</p>
+          </div>
+          <div>
+            <label>Notes</label>
+            <p>{job?.notes}</p>
+          </div>
+          </>
+        )
+      })}
+    </div>
   )
 }
 
