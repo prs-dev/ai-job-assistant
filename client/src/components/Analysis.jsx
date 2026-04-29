61c0cb2cadbe7b7b9f928745d0a2c3ba36ca6e75
prs-dev <pratyushsri.25@gmail.com>
2026-04-29 18:27:13 +0530

    made analyse page

--- client/src/components/Analysis.jsx
+++ client/src/components/Analysis.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Textarea } from './ui/textarea'
+import { Button } from './ui/button'
 
 const Analysis = () => {
   return (
-    <div>Analysis</div>
+    <div className='w-full p-2 flex flex-col gap-[10px]'>
+        <h2>AI Analysis</h2>
+        <div>
+            <Textarea className="w-full h-[200px]" placeholder="job description to analyse"/>
+        </div>
+        <div>
+            <Button>Analyse</Button>
+        </div>
+    </div>
   )
 }
 
